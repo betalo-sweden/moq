b50691268416ccf90a612eeaa21a761831aa2ddc
HaraldNordgren <haraldnordgren@gmail.com>
2018-02-17 15:07:21 +0100

    create regression test

---
+++ pkg/moq/testpackages/folder_named_differently_from_folder/example.go
@@ -0,0 +1,14 @@
+package p1
+
+// PersonStore stores people.
+type PersonStore interface {
+	Get() *Person
+}
+
+// Person is a person.
+type Person struct {
+	ID      string
+	Name    string
+	Company string
+	Website string
+}
